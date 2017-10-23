8f215177831849db28138b20b12e908aeda8b80e
zhengqingxin <362965772@qq.com>
2017-10-23 14:50:15 +0800

    update

--- metadata.js
+++ metadata.js
@@ -18,7 +18,6 @@ module.exports = {
     "src/**/*.vue",
     "src/assets/*.png",
     "www/static/public/favicon.ico",
-    "www/static/public/thinkjs.png"
   ],
   "filesignore": [
     "src/controller/index.tpl.js",
